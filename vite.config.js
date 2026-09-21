f11130f05c23211b8329ef87c9f1d72316261b3b
Suficks <vdo-sofia@mail.ru>
2026-09-21 19:43:47 +0700

    fix config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite';
 
-// base нужен для публикации на GitHub Pages по адресу
-// https://suficks.github.io/Teaboom-Test-Task/
 export default defineConfig({
-  base: '/Teaboom-Test-Task/',
+  base: './',
 });
